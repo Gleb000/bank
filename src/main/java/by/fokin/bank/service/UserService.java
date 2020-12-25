1174c821a9585cf79869642a8bcd4965b3a4be6b
Gleb000 <glebfox3@gmail.com>
2020-12-25 15:26:35 +0300

    Seven commit

--- src/main/java/by/fokin/bank/service/UserService.java
+++ src/main/java/by/fokin/bank/service/UserService.java
@@ -4,7 +4,6 @@ import by.fokin.bank.domain.Role;
 import by.fokin.bank.domain.User;
 import by.fokin.bank.repos.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
